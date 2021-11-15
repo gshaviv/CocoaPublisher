37d7eee643ea539d41e27c6e61c9a15828c83bd3
Guy Shaviv <guy@tivstudio.com>
2021-11-15 12:46:30 +0200

    expose updateItems to try fix edit issue

--- Sources/CocoaPublisher/ConfigurableTableDataSource.swift
+++ Sources/CocoaPublisher/ConfigurableTableDataSource.swift
@@ -18,15 +18,20 @@ public class ConfigurableTableViewDataSource<SectionIdentifierType, ItemIdentifi
             NotificationQueue.default.enqueue(Notification(name: updateItemsNotification, object: self, userInfo: nil), postingStyle: .whenIdle, coalesceMask: [.onName, .onSender], forModes: nil)
         }
     }
+    private(set) public var needsUpdateItems = false
     
     private var updateItemsNotification = Notification.Name("updateItems")
     
     public override init(tableView: UITableView, cellProvider: @escaping UITableViewDiffableDataSource<SectionIdentifierType, ItemIdentifierType>.CellProvider) {
         super.init(tableView: tableView, cellProvider: cellProvider)
-        NotificationCenter.default.addObserver(self, selector: #selector(updateItems), name: updateItemsNotification, object: self)
+        NotificationCenter.default.addObserver(self, selector: #selector(updateItemsIfNeeded), name: updateItemsNotification, object: self)
     }
     
-    @objc private func updateItems() {
+    @objc public func updateItemsIfNeeded() {
+        guard needsUpdateItems else { return }
+        defer {
+            needsUpdateItems = false
+        }
         var snap = NSDiffableDataSourceSnapshot<SectionIdentifierType, ItemIdentifierType>()
         snap.appendSections(sections)
         for (section, items) in items {
@@ -35,51 +40,51 @@ public class ConfigurableTableViewDataSource<SectionIdentifierType, ItemIdentifi
         apply(snap, animatingDifferences: true)
     }
     
-    private var canEditRowBlock: ((UITableView, IndexPath) -> Bool)?
+    private var canEditRowBlock: ((IndexPath) -> Bool)?
     public override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        canEditRowBlock?(tableView, indexPath) ?? false
+        canEditRowBlock?(indexPath) ?? false
     }
-    public func canEditRow(_ b: @escaping (UITableView, IndexPath) -> Bool) {
+    public func canEditRow(_ b: @escaping (IndexPath) -> Bool) {
         canEditRowBlock = b
     }
     
-    private var titleForHeader: ((UITableView, SectionIdentifierType) -> String?)?
+    private var titleForHeader: ((_ section: SectionIdentifierType) -> String?)?
     public override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        titleForHeader?(tableView, sections[section])
+        titleForHeader?(sections[section])
     }
-    public func titleForHeaderInSection(_ b: @escaping (UITableView, SectionIdentifierType) -> String?) {
+    public func titleForHeaderInSection(_ b: @escaping (SectionIdentifierType) -> String?) {
         titleForHeader = b
     }
     
-    private var titleForFooter: ((UITableView, SectionIdentifierType) -> String?)?
+    private var titleForFooter: ((_ section: SectionIdentifierType) -> String?)?
     public override func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
-        titleForFooter?(tableView, sections[section])
+        titleForFooter?(sections[section])
     }
-    public func titleForFooterInSection(_ b: @escaping (UITableView, SectionIdentifierType) -> String?) {
+    public func titleForFooterInSection(_ b: @escaping (SectionIdentifierType) -> String?) {
         titleForFooter = b
     }
     
-    private var commitEditing: ((UITableView, UITableViewCell.EditingStyle, IndexPath) -> Void)?
+    private var commitEditing: ((UITableViewCell.EditingStyle, IndexPath) -> Void)?
     public override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        commitEditing?(tableView, editingStyle, indexPath)
+        commitEditing?(editingStyle, indexPath)
     }
-    public func commitEdit(_ b: @escaping (UITableView, UITableViewCell.EditingStyle, IndexPath) -> Void) {
+    public func commitEdit(_ b: @escaping (UITableViewCell.EditingStyle, IndexPath) -> Void) {
         commitEditing = b
     }
     
-    private var canMoveRowBlock: ((UITableView, IndexPath) -> Bool)?
+    private var canMoveRowBlock: ((IndexPath) -> Bool)?
     public override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        canMoveRowBlock?(tableView, indexPath) ?? false
+        canMoveRowBlock?(indexPath) ?? false
     }
-    public func canMoveRow(_ b: @escaping (UITableView, IndexPath) -> Bool) {
+    public func canMoveRow(_ b: @escaping (IndexPath) -> Bool) {
         canMoveRowBlock = b
     }
     
-    private var moveRowBlock: ((UITableView, IndexPath, IndexPath) -> Void)?
+    private var moveRowBlock: ((IndexPath, IndexPath) -> Void)?
     public override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
-        moveRowBlock?(tableView, sourceIndexPath, destinationIndexPath)
+        moveRowBlock?(sourceIndexPath, destinationIndexPath)
     }
-    public func moveRow(_ b: @escaping (_ tableView: UITableView, _ sourceIndexPath: IndexPath, _ destinationIndexPath: IndexPath) -> Void) {
+    public func moveRow(_ b: @escaping (_ sourceIndexPath: IndexPath, _ destinationIndexPath: IndexPath) -> Void) {
         moveRowBlock = b
     }
     
